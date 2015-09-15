8699e2703cc68669755bdd20b522bc6c87d2afbf
Sangwoo Im <saim80@me.com>
2015-09-14 23:28:19 -0700

    NEW: tvOS target added.

---
+++ SDWebImage+TV/SDWebImage_TV.h
@@ -0,0 +1,13 @@
+//
+//  SDWebImage_TV.h
+//  SDWebImage+TV
+//
+//  Created by Sangwoo Im on 9/14/15.
+//  Copyright © 2015 Dailymotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDWebImage_TV : NSObject
+
+@end
---
+++ SDWebImage+TV/SDWebImage_TV.m
@@ -0,0 +1,13 @@
+//
+//  SDWebImage_TV.m
+//  SDWebImage+TV
+//
+//  Created by Sangwoo Im on 9/14/15.
+//  Copyright © 2015 Dailymotion. All rights reserved.
+//
+
+#import "SDWebImage_TV.h"
+
+@implementation SDWebImage_TV
+
+@end
